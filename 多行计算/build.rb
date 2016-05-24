bf74e04f7aed7c81989b5b8b481ec5d4ffe41b06
fushang318 <fushang318@gmail.com>
2016-05-24 10:31:26 +0800

    修改多行计算

--- 多行计算/build.rb
+++ 多行计算/build.rb
@@ -13,57 +13,57 @@ def generate_float(min_len, max_len)
   (strs*"").to_f
 end
 
-def build_one_content_fix_minus(x, y, data)
-  count = x/3 + y/3
+def build_one_content_fix_minus(hang, lie, data)
+  count = hang/3 + lie/3
 
-  xs = (0..x-1).to_a.sample(count)
-  ys = (0..y-1).to_a.sample(count)
+  hangs = (0..hang-1).to_a.sample(count)
+  lies = (0..lie-1).to_a.sample(count)
 
   0.upto(count-1) do |i|
-    point_x = xs[i]
-    point_y = ys[i]
-    data[point_x][point_y] = -(data[point_x][point_y])
+    point_hang = hangs[i]
+    point_lie = lies[i]
+    data[point_hang][point_lie] = -(data[point_hang][point_lie])
   end
 
   data
 end
 
-def build_one_content(x, y)
+def build_one_content(hang, lie)
   data = []
-  0.upto(x-1) do |_x|
-    data[_x] = []
-    0.upto(y-1) do |_y|
-      data[_x][_y] = generate_float(3, 6)
+  0.upto(hang-1) do |_hang|
+    data[_hang] = []
+    0.upto(lie-1) do |_lie|
+      data[_hang][_lie] = generate_float(3, 6)
     end
   end
-  build_one_content_fix_minus(x, y, data)
+  build_one_content_fix_minus(hang, lie, data)
 end
 
-def build_one_answer(x, y, data)
+def build_one_answer(hang, lie, data)
   # 计算结果
   answer = {}
 
   # 计算每个横行的和
-  0.upto(y-1) do |_y|
+  0.upto(hang-1) do |_hang|
     arr = []
-    0.upto(x-1) do |_x|
-      arr.push data[_x][_y]
+    0.upto(lie-1) do |_lie|
+      arr.push data[_hang][_lie]
     end
 
-    answer["sum,#{_y}"] = arr.inject(0) do |sum, num|
+    answer["#{_hang},sum"] = arr.inject(0) do |sum, num|
       _sum = BigDecimal.new(num.to_s) + BigDecimal.new(sum.to_s)
       _sum.to_f
     end
   end
 
   # 计算每个纵列的和
-  0.upto(x-1) do |_x|
+  0.upto(lie-1) do |_lie|
     arr = []
-    0.upto(y-1) do |_y|
-      arr.push data[_x][_y]
+    0.upto(hang-1) do |_hang|
+      arr.push data[_hang][_lie]
     end
 
-    answer["#{_x},sum"] = arr.inject(0) do |sum, num|
+    answer["sum,#{_lie}"] = arr.inject(0) do |sum, num|
       _sum = BigDecimal.new(num.to_s) + BigDecimal.new(sum.to_s)
       _sum.to_f
     end
@@ -71,9 +71,9 @@ def build_one_answer(x, y, data)
 
   # 计算总和
   arr = []
-  0.upto(x-1) do |_x|
-    0.upto(y-1) do |_y|
-      arr.push data[_x][_y]
+  0.upto(hang-1) do |_hang|
+    0.upto(lie-1) do |_lie|
+      arr.push data[_hang][_lie]
     end
   end
   answer["sum"] = arr.inject(0) do |sum, num|
@@ -83,22 +83,22 @@ def build_one_answer(x, y, data)
   answer
 end
 
-def build_one(x, y)
-  data = build_one_content(x, y)
+def build_one(hang, lie)
+  data = build_one_content(hang, lie)
   {
     content: data,
-    answer: build_one_answer(x,y, data)
+    answer: build_one_answer(hang , lie, data)
   }
 end
 
-# 6 纵列
-x = 6
 # 10 行
-y = 10
+hang = 10
+# 6 列
+lie = 6
 
 result = []
 1.upto(1) do
-  result.push build_one(x, y)
+  result.push build_one(hang, lie)
 end
 
 json_path = File.join(File.expand_path("../", __FILE__), "build.json")
